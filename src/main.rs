38af596815be4f7668a12f9509d1ffe7e6861755
Bulat Kurbanov <kurbanovbul@gmail.com>
2024-05-06 23:39:45 +0200

    Fix

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,11 @@ pub mod updater;
 
 use axum::{http::HeaderMap, routing::post, Router};
 use sentry::{integrations::debug_images::DebugImagesIntegration, types::Dsn, ClientOptions};
+use sentry_tracing::EventFilter;
 use std::{net::SocketAddr, str::FromStr};
+use tower_http::trace::{self, TraceLayer};
+use tracing::Level;
+use tracing_subscriber::{filter, layer::SubscriberExt, util::SubscriberInitExt};
 
 async fn update(headers: HeaderMap) -> &'static str {
     let config_api_key = config::CONFIG.api_key.clone();
@@ -33,8 +37,6 @@ async fn update(headers: HeaderMap) -> &'static str {
 
 #[tokio::main]
 async fn main() {
-    env_logger::init();
-
     let options = ClientOptions {
         dsn: Some(Dsn::from_str(&config::CONFIG.sentry_dsn).unwrap()),
         default_integrations: false,
@@ -44,7 +46,22 @@ async fn main() {
 
     let _guard = sentry::init(options);
 
-    let app = Router::new().route("/update", post(update));
+    let sentry_layer = sentry_tracing::layer().event_filter(|md| match md.level() {
+        &tracing::Level::ERROR => EventFilter::Event,
+        _ => EventFilter::Ignore,
+    });
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer().with_target(false))
+        .with(filter::LevelFilter::INFO)
+        .with(sentry_layer)
+        .init();
+
+    let app = Router::new().route("/update", post(update)).layer(
+        TraceLayer::new_for_http()
+            .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+            .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+    );
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
 
